c3fd7eade8257191255b2189202ca96a0f2e831f
alehkiz <alexandresoad@hotmail.com>
2022-01-23 23:59:58 -0300

    add date picker

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 from core import get_dataframe, get_totals, get_new, populate_graphics, populate_metrics
 from utils import format_date
+import datetime
 
 df = get_dataframe()
 
@@ -19,6 +20,10 @@ hide_menu_style = """
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 
+
+
+
+
 #TODO: Traduzir os nomes dos países
 if not df is False:
     df.fillna(0, inplace=True)
@@ -28,7 +33,49 @@ if not df is False:
     if not not st_contries:
         df = df[df['country_pt'].isin(st_contries)]
     df = df.sort_values(by='date', ascending=False)
+    dates = [pd.to_datetime(_).date() for _ in df['date'].unique()]
+    # start_time = st.slider(
+    #  "When do you start?",
+    #  min_value=df.iloc[0]['date'].date(),
+    #  max_value=df.iloc[-1]['date'].date(),
+    #  format="DD/MM/YYYY")
+    # st.write("Start time:", start_time)
+
+    # start_time, end_time = st.select_slider(
+    #     'Selecione o período para os gráficos',
+    #     options=dates,
+
+    #     # value=(df.iloc[0]['date'].date(), df.iloc[1]['date'].date())
+    # )
+    # st.slider(
+    #     'Selecione data',
+    #     min_value=df.iloc[-1]['date'].date(),
+    #     max_value=df.iloc[0]['date'].date() + datetime.timedelta(days=1),
+    # )
+    # print(dates)
+    # date_range = st.select_slider(
+    #     'Período',
+    #     options=dates,
+    #     value=(df.iloc[-1]['date'].date(),df.iloc[0]['date'].date() + datetime.timedelta(days=1)),
+
+    #     )
+    # st.write('Valor de data', date_range)
+    # start_color, end_color = st.select_slider(
+    #  'Select a range of color wavelength',
+    #  options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'],
+    #  value=('red', 'blue'))
+    # st.write('You selected wavelengths between', start_color, 'and', end_color)
+
+
+    
     populate_metrics(df)
+    min_value=df.iloc[-1]['date']
+    max_value=df.iloc[0]['date']
+    c1, c2 = st.columns(2)
+    init_date = c1.date_input("Data inicial", min_value)
+    end_date = c2.date_input('Data final', max_value)
+    print(init_date)
+    df = df[(df['date'] >= pd.to_datetime(init_date)) & (df['date'] <= pd.to_datetime(end_date))]
     populate_graphics(df)
 else:
     st.title('Dados sendo carregados...')
